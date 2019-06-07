e29c72f0faa94a2396905f5dad68bdb5e7eeb7d2
Seán Kelly <daxorinator@gmail.com>
2019-06-07 20:41:46 +0100

    Link deps

--- build.rs
+++ build.rs
@@ -1,4 +1,7 @@
 fn main() {
-    println!("cargo:rustc-link-search=native=C:/devkitPro/MLlib/lib");
+    println!("cargo:rustc-link-search=native=c:/devkitPro/MLlib/lib");
     println!("cargo:rustc-link-lib=static=MLlib");
+    println!("cargo:rustc-link-search=native=c:/devkitPro/libogc/lib/wii");
+    println!("cargo:rustc-link-lib=static=wiiuse");
+    println!("cargo:rustc-link-lib=static=ogc");
 }
